f8c4e495c5737294cb97a0e678684be0c32febf0
gabriel.rodrigues.mota <gabriel.rodrigues.mota@outlook.com>
2022-09-19 16:15:53 -0300

    Finalizando curso de DDD

--- src/domain/repository/order-repository-interface.ts
+++
@@ -1,5 +0,0 @@
-import RepositoryInterface from "../repository/repository-interface";
-import Customer from "../entity/customer";
-
-export default interface CustomerRepositoryInterface
-    extends RepositoryInterface<Customer> { }
